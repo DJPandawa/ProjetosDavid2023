63189798245cce4b148522dbd8c91e2088098055
david <david.dsgg@gmail.com>
2023-04-04 14:29:23 -0300

    Refatoração da Classe Controller.

--- AppListaNova/app/src/main/java/devanandroid/david/applistanova/controller/PessoaController.java
+++ AppListaNova/app/src/main/java/devanandroid/david/applistanova/controller/PessoaController.java
@@ -1,25 +1,62 @@
 package devanandroid.david.applistanova.controller;
 
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import devanandroid.david.applistanova.model.Pessoa;
+import devanandroid.david.applistanova.view.MainActivity;
 
 public class PessoaController {
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor listaVip;
+    public static final String NOME_PREFERENCES = "pref_listaVip";
+
+    public PessoaController(MainActivity mainActivity){
+        preferences = mainActivity.getSharedPreferences(NOME_PREFERENCES, 0);
+        listaVip = preferences.edit();
+    }
+
 
     @NonNull
     @Override
     public String toString() {
 
-        Log.d("MVC_Controller","Controller Iniciada...");
+        Log.d("MVC_Controller", "Controller Iniciada...");
 
         return super.toString();
     }
 
     public void salvar(Pessoa pessoa) {
 
-        Log.d("MVC_Controller","Salvo: "+pessoa.toString());
+        Log.d("MVC_Controller", "Salvo: " + pessoa.toString());
+
+        listaVip.putString("primeiroNome", pessoa.getPrimeiroNome());
+        listaVip.putString("sobreNome", pessoa.getSobreNome());
+        listaVip.putString("telefoneContato", pessoa.getTelefoneContato());
+        listaVip.putString("nomeCurso", pessoa.getCursoDesejado());
+        listaVip.apply();
 
     }
-}
+
+    public Pessoa buscar(Pessoa pessoa) {
+
+        pessoa.setPrimeiroNome(preferences.getString("primeiroNome", "N_A"));
+        pessoa.setSobreNome(preferences.getString("sobreNome", "N_A"));
+        pessoa.setTelefoneContato(preferences.getString("telefoneContato", "N_A"));
+        pessoa.setCursoDesejado(preferences.getString("nomeCurso", "N_A"));
+
+        return pessoa;
+    }
+
+    public void limpar() {
+
+         listaVip.clear();
+          listaVip.apply();
+
+    }
+
+    }
+
